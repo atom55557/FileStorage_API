f59e68ab74161d5bcc7881ac5249dae7dd3ad3f7
atom55557 <serhat.yua@gmail.com>
2026-07-04 07:49:28 +0300

    add download and delete

--- storage/src/main/java/com/example/storage/controller/FileController.java
+++ storage/src/main/java/com/example/storage/controller/FileController.java
@@ -2,15 +2,21 @@ package com.example.storage.controller;
 
 import com.example.storage.entity.FileMetadata;
 import com.example.storage.service.FileService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/files")
@@ -18,6 +24,12 @@ import java.io.IOException;
 public class FileController {
 
     private final FileService fileService;
+
+    @GetMapping
+    public ResponseEntity<List<FileMetadata>> getAll(){
+        List<FileMetadata> files = fileService.getAllFiles();
+        return ResponseEntity.ok(files);
+    }
 
     @PostMapping("/upload")
     public ResponseEntity<?> uploadFile(@RequestParam("file" ) MultipartFile file){
@@ -28,4 +40,46 @@ public class FileController {
                 return ResponseEntity.status(500).body("Dosya yüklenirken hata oluştu: " + e.getMessage());
             }
     }
+
+    @GetMapping("/download/{id}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable Long id){
+        try {
+            Resource resource = fileService.loadFileAsResource(id);
+
+            // Dosyanın içerik tipini (MIME) tespit etme
+            Path path = Paths.get(resource.getFile().getAbsolutePath());
+            String contentType = Files.probeContentType(path);
+            if (contentType == null) {
+                contentType = "application/octet-stream"; // Bilinmeyen türler için genel binary formatı
+            }
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.parseMediaType(contentType))
+                    // attachment; filename="..." ifadesi tarayıcıya indirme penceresi açtırır
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename().substring(37) + "\"")
+                    //.substring(37) UUID yapısının (36 karakter + alt tire) adını temizleyip orijinal adı indirmeyi sağlar.
+                    .body(resource);
+
+        } catch (MalformedURLException e) {
+            return ResponseEntity.status(500).build();
+        } catch (IOException e) {
+            return ResponseEntity.status(404).build();
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(404).build();
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteFile(@PathVariable Long id) {
+        try {
+            fileService.deleteFile(id);
+            return ResponseEntity.ok("Dosya başarıyla silindi. ID: " + id);
+        } catch (IOException e) {
+            return ResponseEntity.status(500).body("Dosya diskten silinirken hata oluştu.");
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(404).body(e.getMessage());
+        }
+    }
+
+
 }
--- storage/src/main/java/com/example/storage/service/FileService.java
+++ storage/src/main/java/com/example/storage/service/FileService.java
@@ -4,14 +4,18 @@ import com.example.storage.entity.FileMetadata;
 import com.example.storage.repository.FileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -22,6 +26,11 @@ public class FileService {
     private String uploadDir;
 
     private final FileRepository fileRepository;
+
+    public List<FileMetadata> getAllFiles(){
+        return fileRepository.findAll();
+    }
+
     public FileMetadata saveFile(MultipartFile file) throws IOException {
         // Yükleme klasörü yoksa oluştur
         File directory = new File(uploadDir);
@@ -44,6 +53,38 @@ public class FileService {
         metadata.setFilePath(filePath.toString());
 
         return fileRepository.save(metadata);
+    }
+    //Dosya İndirme Metodu
+    public Resource loadFileAsResource(long id) throws MalformedURLException {
+        // 1. Veritabanından dosya bilgilerini getir, bulamazsa hata fırlat
+        FileMetadata metadata = fileRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Dosya veritabanında bulunamadı. ID:"+id));
+
+        // 2. Dosyanın diskteki yolunu al ve Resource nesnesine çevir
+        Path filePath = Paths.get(metadata.getFilePath());
+        Resource resource = new UrlResource(filePath.toUri());
+
+        // 3. Dosya gerçekten diskte var mı kontrol et
+        if (resource.exists() && resource.isReadable()) {
+            return resource;
+        } else {
+            throw new RuntimeException("Dosya diskte fiziksel olarak bulunamadı veya okunamıyor.");
+        }
+
+    }
+
+    // Dosya Silme Metodu
+    public void deleteFile(Long id) throws IOException {
+        // 1. Veritabanından bilgileri getir
+        FileMetadata metadata = fileRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Silinecek dosya bulunamadı. ID: " + id));
+
+        // 2. Önce Windows diskindeki fiziksel dosyayı sil
+        Path filePath = Paths.get(metadata.getFilePath());
+        Files.deleteIfExists(filePath);
+
+        // 3. Sonra veritabanındaki kaydı sil
+        fileRepository.delete(metadata);
     }
 
 }
